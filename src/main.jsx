359f9f5f1e349532545fcac9ee02f5ce04d02999
Hajizada Farid <faridmh@code.edu.az>
2024-08-08 15:31:33 +0400

    feat: add GalleryArea

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>

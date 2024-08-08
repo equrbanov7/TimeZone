3998c9f95e65af055ed7d3bea3e61640d622e847
Hajizada Farid <faridmh@code.edu.az>
2024-08-08 16:29:00 +0400

    Update server port and API URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "http://localhost:5001";
 
 export const instance = axios.create({
     baseURL: "http://localhost:5000",
